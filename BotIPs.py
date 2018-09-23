2c56cc75526d08d0aed53f0e708aac92599d7099
daniel-britt <13905361+daniel-britt@users.noreply.github.com>
2018-09-23 13:33:58 -0600

    Update BotIPs.py

--- BotIPs.py
+++ BotIPs.py
@@ -1,16 +1,1 @@
-import pyperclip
-import re
 
-#a = recurring IP addresses which don't need to be investigated (yandexbot, googlebot, ldsbot, automated access)
-a = []
-x = pyperclip.paste()
-z = ""
-final = ""
-
-#Extract src from WAF_task_related page
-for i in x.splitlines():
-    y = i.strip().split()
-    if y[0] not in a:
-        z += y[0] + "\r\n"
-
-pyperclip.copy(z)
